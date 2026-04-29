49d30bc1c829fa7c28a23edfc9a3d42089ea5809
root <root@DESKTOP-CBQDRR5.localdomain>
2026-04-29 10:03:43 +0000

    new files added

---
+++ git_pull.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Navigate to repo directory
+cd /root/docker_practice/docker_practice
+
+# Pull latest code
+git pull origin master
+
+# Log message
+echo "Code pulled successfully at $(date)"
