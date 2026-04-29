aab2b138a1985b6ad2796cce642285d607f54473
root <root@DESKTOP-CBQDRR5.localdomain>
2026-04-29 10:16:23 +0000

    Revert "text file added"
    This reverts commit c20ffaf467a192604e8d11933f1aa316f4494303.

--- git_pull.sh
+++ git_pull.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Navigate to repo directory
 cd /root/docker_practice/docker_practice
 
-git pull
+# Pull latest code
+git pull origin master
 
+# Log message
 echo "Code pulled successfully at $(date)"
